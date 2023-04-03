7c1fbc02621453e5fb854c86b9e01560415f6fe0
Ayman Alhattami <ayman.m.alhattami@gmail.com>
2023-04-03 19:15:37 +0300

    fix: log system user

--- src/Listeners/ArtisanCommandListener.php
+++ src/Listeners/ArtisanCommandListener.php
@@ -4,10 +4,10 @@ namespace DevMoez\ArtisanCommandGuard\Listeners;
 
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Artisan;
-use Illuminate\Support\Facades\Process;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Console\Events\CommandStarting;
+use Symfony\Component\Process\Process;
 
 class ArtisanCommandListener
 {
@@ -29,16 +29,16 @@ class ArtisanCommandListener
         $message = "$event->command is blocked from running in $appEnv environment.";
 
         if (isset($config['environments'][$appEnv])) {
-            if (in_array($event->command, $commands['environments'][$appEnv])) {
+
+            if (in_array($event->command, $config['environments'][$appEnv])) {
                 # log system user to log file
                 if ($config['enable_log_user']) {
                     $process = new Process(['whoami']);
                     $process->run();
-                    // $user = $process->getOutput();
 
                     Log::critical($message, [
+                        'command'   => $event->command,
                         'user'  => $process->getOutput(),
-                        'command'   => $event->command
                     ]);
                 }
 
